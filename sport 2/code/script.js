83c97196260f1b7fc9fcbdedd7bfd30a20a5cba9
pouyaMK <151400537+pouyaMK@users.noreply.github.com>
2024-05-09 22:50:23 +0330

    Add files via upload

---
+++ sport 2/code/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let $ = document;
+
+let ersal = $.querySelector('.ersal');
+let tazmin = $.querySelector('.tazmin');
+let amniat = $.querySelector('.amniat');
+let sorat = $.querySelector('.sorat');
+let tajrobe = $.querySelector('.tajrobe');
+
+
+// ersal.addEventListener('mouseover', function(){
+//     tazmin.style.backgroundColor = '#000';
+// })
